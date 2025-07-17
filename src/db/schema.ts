2a93a669b9c9c20b2e709bbfb69f6f3fa8045d98
caioafbrito <caio.freitas.brito@developz.com.br>
2025-07-17 14:35:30 -0300

    With all schemas defined

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,28 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, text, timestamp, date, boolean, serial } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 255 }).notNull(),
+  age: integer("age").notNull(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
 });
+
+export const tasks = pgTable("tasks", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  description: text("description"),
+  dueDate: date("due_date", { mode: "string" }),
+  isDone: boolean("is_done").default(false),
+  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
+  owner: integer("owner").notNull().references(() => users.id, { onDelete: "cascade" }),
+});
+
+export const usersRelations = relations(tasks, ({ many }) => ({
+  tasks: many(tasks)
+}));
+
+
