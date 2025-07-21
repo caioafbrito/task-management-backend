d7eb3f6e28ec99e500aaca756d5e18d69b50a93a
caioafbrito <caio.freitas.brito@developz.com.br>
2025-07-20 21:19:19 -0300

    Status code fixed

--- src/middlewares/checks.ts
+++ src/middlewares/checks.ts
@@ -5,5 +5,5 @@ import { ApiError } from "utils/error.js";
 export const checkUserId = async(req: Request, res: Response, next: NextFunction) => {
     const { userId } = req.user;
     const user = await findUserById(userId);
-    if (!user) return next(new ApiError(`User with id ${userId} does not exist.`, 403));
+    if (!user) return next(new ApiError(`User with id ${userId} does not exist.`, 404));
 }
